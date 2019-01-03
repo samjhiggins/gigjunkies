a71559f68e2dd163a403f415e9ff95653a9bc709
Sam Higgins <sam.higgins@prezzybox.com>
2019-01-03 11:59:03 +0000

    tags

--- wp-content/themes/blankslate/tag.php
+++ wp-content/themes/blankslate/tag.php
@@ -6,14 +6,16 @@
 		<header class="header">
 
 
-<h1 class="entry-title"><?php _e( 'Tag Archives: ', 'gigjunkies' ); ?><?php single_tag_title(); ?></h1>
+<h1 class="entry-title"><?php _e( 'Tag: ', 'gigjunkies' ); ?><?php single_tag_title(); ?></h1>
 </header>
 
+<div class="entry-grid-list">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="col-md-4 col-sm-6">
 	<?php get_template_part( 'entry' ); ?>
 	</div>
 <?php endwhile; endif; ?>
+</div>
 <?php get_template_part( 'nav', 'below' ); ?>
 </div>
 </div>
